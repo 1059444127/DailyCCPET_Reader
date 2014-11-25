da003dc9cb9d607defe7fa98a7970de678668fcc
JCRodicio <jcortes.rodicio@gmail.com>
2014-11-25 18:42:31 +0100

    DailyCCPET_Reader
    This software reads the *.log files generated after the daily quality
    control of a PET/CT Philips GEMINI/TF and rewrites the sumarized results
    in diferent files: EnergyResolution, TimeResolution, GainPMT and
    EmisionTest. This allows to follow up the results

---
+++ DailyCCPET_Reader/Form1.Designer.vb
@@ -0,0 +1,240 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LstFiles = New System.Windows.Forms.ListBox()
+        Me.BtnAgregar = New System.Windows.Forms.Button()
+        Me.BtnProcesar = New System.Windows.Forms.Button()
+        Me.BtnClearLst = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'LstFiles
+        '
+        Me.LstFiles.FormattingEnabled = True
+        Me.LstFiles.Location = New System.Drawing.Point(12, 12)
+        Me.LstFiles.Name = "LstFiles"
+        Me.LstFiles.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended
+        Me.LstFiles.Size = New System.Drawing.Size(481, 290)
+        Me.LstFiles.TabIndex = 0
+        '
+        'BtnAgregar
+        '
+        Me.BtnAgregar.Location = New System.Drawing.Point(12, 307)
+        Me.BtnAgregar.Name = "BtnAgregar"
+        Me.BtnAgregar.Size = New System.Drawing.Size(106, 34)
+        Me.BtnAgregar.TabIndex = 1
+        Me.BtnAgregar.Text = "Agregar archivos"
+        Me.BtnAgregar.UseVisualStyleBackColor = True
+        '
+        'BtnProcesar
+        '
+        Me.BtnProcesar.Location = New System.Drawing.Point(12, 387)
+        Me.BtnProcesar.Name = "BtnProcesar"
+        Me.BtnProcesar.Size = New System.Drawing.Size(106, 63)
+        Me.BtnProcesar.TabIndex = 2
+        Me.BtnProcesar.Text = "Procesar"
+        Me.BtnProcesar.UseVisualStyleBackColor = True
+        '
+        'BtnClearLst
+        '
+        Me.BtnClearLst.Location = New System.Drawing.Point(12, 347)
+        Me.BtnClearLst.Name = "BtnClearLst"
+        Me.BtnClearLst.Size = New System.Drawing.Size(106, 34)
+        Me.BtnClearLst.TabIndex = 3
+        Me.BtnClearLst.Text = "Eliminar todo"
+        Me.BtnClearLst.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(159, 407)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(334, 20)
+        Me.TextBox1.TabIndex = 4
+        Me.TextBox1.Visible = False
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(409, 307)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(84, 20)
+        Me.TextBox2.TabIndex = 5
+        Me.TextBox2.Text = "ERes.txt"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(409, 332)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(84, 20)
+        Me.TextBox3.TabIndex = 6
+        Me.TextBox3.Text = "TempRes.txt"
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(131, 425)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(362, 24)
+        Me.ProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous
+        Me.ProgressBar1.TabIndex = 7
+        Me.ProgressBar1.Visible = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(128, 410)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Label1"
+        Me.Label1.Visible = False
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(409, 358)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(84, 20)
+        Me.TextBox4.TabIndex = 9
+        Me.TextBox4.Text = "GainPMT.txt"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(409, 381)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(84, 20)
+        Me.TextBox5.TabIndex = 10
+        Me.TextBox5.Text = "EmisTest.txt"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(302, 310)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(101, 13)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Resolución Energía"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(296, 335)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(107, 13)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Resolución Temporal"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(324, 361)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(79, 13)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Ganancia PMT"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(336, 384)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(67, 13)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "Test Emisión"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(138, 384)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(51, 13)
+        Me.Label6.TabIndex = 16
+        Me.Label6.Text = "TestTime"
+        Me.Label6.Visible = False
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(211, 381)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(84, 20)
+        Me.TextBox6.TabIndex = 15
+        Me.TextBox6.Text = "TestTime.txt"
+        Me.TextBox6.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(505, 461)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.BtnClearLst)
+        Me.Controls.Add(Me.BtnProcesar)
+        Me.Controls.Add(Me.BtnAgregar)
+        Me.Controls.Add(Me.LstFiles)
+        Me.Name = "Form1"
+        Me.Text = "Daily CCPET Reader"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents LstFiles As System.Windows.Forms.ListBox
+    Friend WithEvents BtnAgregar As System.Windows.Forms.Button
+    Friend WithEvents BtnProcesar As System.Windows.Forms.Button
+    Friend WithEvents BtnClearLst As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents TextBox6 As System.Windows.Forms.TextBox
+
+End Class
---
+++ DailyCCPET_Reader/Form1.vb
@@ -0,0 +1,327 @@
+﻿Public Class Form1
+    Dim workPath As String
+
+    Private Sub BtnAgregar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnAgregar.Click
+
+        Dim DlgAbrir As New OpenFileDialog()
+        Dim ruta As String
+        DlgAbrir.Multiselect = True
+        If (DlgAbrir.ShowDialog() = Windows.Forms.DialogResult.OK) Then
+            For Each ruta In DlgAbrir.FileNames
+                LstFiles.Items.Add(ruta)
+            Next
+        End If
+        TextBox1.Text = IO.Path.GetDirectoryName(ruta)
+        workPath = IO.Path.GetDirectoryName(ruta)
+    End Sub
+
+    Private Sub BtnClearLst_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnClearLst.Click
+
+        LstFiles.Items.Clear()
+        LstFiles.Refresh()
+        TextBox1.Text = LstFiles.Items.Count
+    End Sub
+
+    Private Sub BtnProcesar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnProcesar.Click
+        Dim path_Eres As String
+        Dim path_TempRes As String
+        Dim path_GainPMT As String
+        Dim path_EmisTest As String
+        Dim path_TestTime As String
+        path_Eres = TextBox2.Text
+        path_GainPMT = TextBox4.Text
+        path_TempRes = TextBox3.Text
+        path_EmisTest = TextBox5.Text
+        path_TestTime = TextBox6.Text
+
+
+        With ProgressBar1
+            .Visible = True
+            .Maximum = 5
+            .Minimum = 0
+            .Value = 0
+        End With
+
+        WriteERes(path_Eres)
+        Refresh()
+        WriteTempRes(path_TempRes)
+        Refresh()
+        WriteGainPMT(path_GainPMT)
+        Refresh()
+        WriteEmisTest(path_EmisTest)
+        Refresh()
+        WriteTimeTest(path_TestTime)
+        Refresh()
+
+    End Sub
+
+    Private Function WriteERes(ByVal fich_Eres As String)
+        Dim path_orig As String
+        Dim findText As String
+        Dim fileString As String
+        Dim posEnergy As Integer
+        Dim posFecha As Integer
+        Dim fecha As String = ""
+        Dim posEres As Integer
+
+        Dim Eres, Emin, Emax As String
+        Dim CentMed, CentMin, CentMax As String
+
+        findText = "Prueba de energ"
+        With Label1
+            .Visible = True
+            .Text = "Resolución Energia"
+            'Refresh()
+        End With
+        ProgressBar1.Value = 1
+        'Refresh()
+        Dim sw As New System.IO.StreamWriter(workPath + "\" + fich_Eres, True)
+        sw.WriteLine("Challenge Accepted!!!")
+        sw.WriteLine("Fecha : ERes (%) : Emin (%) : Emax (%) : CentMed : CentMin : CentMax")
+
+        For i = 0 To LstFiles.Items.Count - 1
+            path_orig = LstFiles.Items.Item(i)
+            fileString = My.Computer.FileSystem.ReadAllText(path_orig)
+            posEnergy = InStr(fileString, findText)
+            'With ProgressBar1
+            ' .Visible = True
+            ' .Maximum = LstFiles.Items.Count - 1
+            ' .Minimum = 0
+            ' .Value = i
+            ' End With
+            If posEnergy <> 0 Then
+                findText = "Hora de inicio"
+                posFecha = InStr(fileString, findText)
+                fecha = fileString.Substring(posFecha + 32, 10)
+                'TextBox1.Text = fecha
+                sw.Write(fecha + vbTab)
+                findText = "98 <= (Media Centroide)"
+                posEres = InStr(fileString, findText)
+                '174 son los caracteres desde el inicio de 'fintText hasta justo el comienzo de Eres
+                'son 171 caracteres + 4 saltos de linea - 1
+                Eres = fileString.Substring(posEres + 174, 5)
+                Emin = fileString.Substring(posEres + 217, 5)
+                Emax = fileString.Substring(posEres + 260, 5)
+                CentMed = fileString.Substring(posEres + 163, 5)
+                CentMin = fileString.Substring(posEres + 206, 5)
+                CentMax = fileString.Substring(posEres + 249, 5)
+                'TextBox1.Text = fecha
+                sw.Write(Eres + vbTab + Emin + vbTab + Emax + vbTab + CentMed + vbTab + CentMin + vbTab + CentMax + vbCrLf)
+            End If
+        Next
+        sw.Close()
+
+        'Refresh()
+        'Label1.Visible = False
+
+    End Function
+
+    Private Function WriteTempRes(ByVal fich_TempRes As String)
+        Dim path_orig As String
+        Dim findText As String
+        Dim fileString As String
+        Dim posicion As Integer
+        Dim posFecha As Integer
+        Dim fecha As String = ""
+        Dim posEres As Integer
+        Dim Eres As String
+
+        findText = "Prueba de tiempo"
+        With Label1
+            .Visible = True
+            .Text = "Resolución Temporal"
+            'Refresh()
+        End With
+        ProgressBar1.Value = 2
+        'Refresh()
+        Dim sw As New System.IO.StreamWriter(workPath + "\" + fich_TempRes, True)
+        sw.WriteLine("Challenge Accepted!!!")
+        sw.WriteLine("Fecha : TempRes (ps)")
+
+        For i = 0 To LstFiles.Items.Count - 1
+            path_orig = LstFiles.Items.Item(i)
+            fileString = My.Computer.FileSystem.ReadAllText(path_orig)
+            posicion = InStr(fileString, findText)
+            'With ProgressBar1
+            ' .Visible = True
+            ' .Maximum = LstFiles.Items.Count - 1
+            ' .Minimum = 0
+            ' .Value = i
+            ' End With
+            If posicion <> 0 Then
+                findText = "Hora de inicio"
+                posFecha = InStr(fileString, findText)
+                fecha = fileString.Substring(posFecha + 32, 10)
+                'TextBox1.Text = fecha
+                sw.Write(fecha + vbTab)
+                findText = "Cambiar < 20.0-ps"
+                posEres = InStr(fileString, findText)
+                Eres = fileString.Substring(posEres + 50, 5)
+                'TextBox1.Text = fecha
+                sw.Write(Eres + vbCrLf)
+            End If
+        Next
+        sw.Close()
+        'ProgressBar1.Value = 2
+        'Refresh()
+        'Label1.Visible = False
+
+    End Function
+
+    Private Function WriteGainPMT(ByVal fich_GainPMT As String)
+        Dim path_orig As String
+        Dim findText As String
+        Dim fileString As String
+        Dim posicion As Integer
+        Dim posFecha As Integer
+        Dim fecha As String = ""
+        Dim posGain As Integer
+
+        Dim iteraciones, ErrMed, ErrMax As String
+        Dim GainMed, GainMin, GainMax As String
+
+        findText = "ganancia de PMT:"
+        With Label1
+            .Visible = True
+            .Text = "Ganancia PMT"
+        End With
+        ProgressBar1.Value = 3
+        'Refresh()
+        Dim sw As New System.IO.StreamWriter(workPath + "\" + fich_GainPMT, True)
+        sw.WriteLine("Challenge Accepted!!!")
+        sw.WriteLine("Fecha : Iter# : ErrMed(%) : ErrMax(%) : GainMed : GainMax : GainMin")
+
+        For i = 0 To LstFiles.Items.Count - 1
+            path_orig = LstFiles.Items.Item(i)
+            fileString = My.Computer.FileSystem.ReadAllText(path_orig)
+            posicion = InStr(fileString, findText)
+            'With ProgressBar1
+            ' .Visible = True
+            ' .Maximum = LstFiles.Items.Count - 1
+            ' .Minimum = 0
+            ' .Value = i
+            ' End With
+            If posicion <> 0 Then
+                findText = "Hora de inicio"
+                posFecha = InStr(fileString, findText)
+                fecha = fileString.Substring(posFecha + 32, 10)
+                'TextBox1.Text = fecha
+                sw.Write(fecha + vbTab)
+                findText = "Factor de ganancia"
+                posGain = InStr(fileString, findText)
+                iteraciones = fileString.Substring(posGain - 207, 2)
+                ErrMed = fileString.Substring(posGain - 164, 5)
+                ErrMax = fileString.Substring(posGain - 113, 6)
+                If ErrMax.Substring(0, 1) <> "-" Then
+                    ErrMax = fileString.Substring(posGain - 113, 5)
+                End If
+                GainMed = fileString.Substring(posGain + 93, 6)
+                GainMin = fileString.Substring(posGain + 132, 6)
+                GainMax = fileString.Substring(posGain + 171, 6)
+                'TextBox1.Text = fecha
+                sw.Write(iteraciones + vbTab + ErrMed + vbTab + ErrMax + vbTab + GainMed + vbTab + GainMin + vbTab + GainMax + vbCrLf)
+            End If
+        Next
+        sw.Close()
+        ProgressBar1.Value = 3
+        Refresh()
+        'Label1.Visible = False
+
+    End Function
+
+    Private Function WriteEmisTest(ByVal fich_EmisTest As String)
+        Dim path_orig As String
+        Dim findText As String
+        Dim fileString As String
+        Dim posicion As Integer
+        Dim posFecha As Integer
+        Dim fecha As String = ""
+        Dim posEmis As Integer
+
+        Dim Lin_3, Lin_2, Lin_1 As String
+        Dim Lin3, Lin2, Lin1, Lin0 As String
+
+        findText = "Prueba de emisiones"
+        With Label1
+            .Visible = True
+            .Text = "Test de emisión"
+        End With
+        Dim sw As New System.IO.StreamWriter(workPath + "\" + fich_EmisTest, True)
+        sw.WriteLine("Challenge Accepted!!!")
+        sw.WriteLine("Fecha : Lin(-3) : Lin(-2) : Lin(-1) : Lin(0) : Lin(1) : Lin(2) : Lin(3)")
+
+        For i = 0 To LstFiles.Items.Count - 1
+            path_orig = LstFiles.Items.Item(i)
+            fileString = My.Computer.FileSystem.ReadAllText(path_orig)
+            posicion = InStr(fileString, findText)
+            'With ProgressBar1
+            ' .Visible = True
+            ' .Maximum = LstFiles.Items.Count - 1
+            ' .Minimum = 0
+            ' .Value = i
+            ' End With
+            If posicion <> 0 Then
+                findText = "Hora de inicio"
+                posFecha = InStr(fileString, findText)
+                fecha = fileString.Substring(posFecha + 32, 10)
+                'TextBox1.Text = fecha
+                sw.Write(fecha + vbTab)
+                findText = "Error de linealidad <= 2.0"
+                posEmis = InStr(fileString, findText)
+                Lin_3 = fileString.Substring(posEmis + 196, 6)
+                Lin_2 = fileString.Substring(posEmis + 244, 6)
+                Lin_1 = fileString.Substring(posEmis + 292, 6)
+                Lin0 = fileString.Substring(posEmis + 340, 6)
+                Lin1 = fileString.Substring(posEmis + 388, 6)
+                Lin2 = fileString.Substring(posEmis + 436, 6)
+                Lin3 = fileString.Substring(posEmis + 484, 6)
+                'TextBox1.Text = fecha
+                sw.Write(Lin_3 + vbTab + Lin_2 + vbTab + Lin_1 + vbTab + Lin0 + vbTab + Lin1 + vbTab + Lin2 + vbTab + Lin3 + vbCrLf)
+            End If
+        Next
+        sw.Close()
+        ProgressBar1.Value = 4
+        'Label1.Visible = False
+
+    End Function
+
+    Private Function WriteTimeTest(ByVal fich_TimeTest As String)
+        Dim path_orig As String
+        Dim findText As String
+        Dim fileString As String
+        Dim posicion As Integer
+        Dim posFecha As Integer
+        Dim fecha As String = ""
+        Dim posTime As Integer
+        Dim Time As String
+
+        findText = "RESUMEN:"
+        With Label1
+            .Visible = True
+            .Text = "Duracion del Test"
+        End With
+        ProgressBar1.Value = 5
+        Dim sw As New System.IO.StreamWriter(workPath + "\" + fich_TimeTest, True)
+        sw.WriteLine("Challenge Accepted!!!")
+        sw.WriteLine("Fecha : Duracion (min)")
+
+        For i = 0 To LstFiles.Items.Count - 1
+            path_orig = LstFiles.Items.Item(i)
+            fileString = My.Computer.FileSystem.ReadAllText(path_orig)
+            posicion = InStr(fileString, findText)
+
+            If posicion <> 0 Then
+                findText = "Hora de inicio"
+                posFecha = InStr(fileString, findText)
+                fecha = fileString.Substring(posFecha + 32, 10)
+                sw.Write(fecha + vbTab)
+                findText = "Tiempo de"
+                posTime = InStr(fileString, findText)
+                Time = fileString.Substring(posTime + 32, 4)
+                sw.Write(Time + vbCrLf)
+            End If
+        Next
+        sw.Close()
+
+    End Function
+End Class
